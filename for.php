adced7a5199920c917b17b8fb9cfb5330ade3360
FaisalAbed44 <faisalalhjori@gmail.com>
2023-12-11 04:12:54 +0300

    first commit

---
+++ for.php
@@ -0,0 +1,11 @@
+<?php
+$family =['faisal','Anas','abid'];
+
+foreach($family as $member){
+    echo $member .'<br>';
+}
+// for ($i=0; $i <count($family) ; $i++) { 
+//     echo $family[$i].'<br>';
+// }
+
+?>
